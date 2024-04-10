13d51cdda1d31a92db9d3a0c71146a4ccaa4d852
joey <joyshen1121@gmail.com>
2024-04-10 10:42:54 +0800

    Remove kindergarten directory

--- kindergarten/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  cloudinary: {
-    cloudName: 'dntokf0em',
-    apiKey: '116245672981179',
-    apiSecret: '9bo6kGJXSKonj9fnw3e4QD2iAeE'
-  }
-};
